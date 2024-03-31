0d78887bf3d497bdec2c250ea2f90e07582c7d09
ofek <ofekh1989@gmail.com>
2024-03-31 13:42:53 +0300

    wip

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
@@ -55,7 +56,10 @@ class MealItem extends StatelessWidget {
                     const SizedBox(height: 12),
                     Row(
                       children: [
-                        
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
                       ],
                     ),
                   ],
